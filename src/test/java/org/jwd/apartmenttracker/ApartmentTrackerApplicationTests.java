a13c1494796b9964500989f182f021783abdbc4c
Jorden Dickerson <jordendickerson24@gmail.com>
2025-09-08 08:29:23 -0600

    Initial Commit

---
+++ src/test/java/org/jwd/apartmenttracker/ApartmentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jwd.apartmenttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
