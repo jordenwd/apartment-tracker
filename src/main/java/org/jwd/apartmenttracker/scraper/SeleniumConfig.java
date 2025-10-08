61717942e023c0e90f0f1823fab06a43c6b143dc
Jorden <jordendickerson24@gmail.com>
2025-10-07 23:26:27 -0600

    Docker setup

--- src/main/java/org/jwd/apartmenttracker/scraper/SeleniumConfig.java
+++ src/main/java/org/jwd/apartmenttracker/scraper/SeleniumConfig.java
@@ -1,17 +1,24 @@
 package org.jwd.apartmenttracker.scraper;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 @Configuration
 public class SeleniumConfig {
+
+    @Value("${selenium.grid.url}")
+    private String seleniumGridUrl;
+
     @Bean
-    public WebDriver driver(){
+    public WebDriver driver() throws MalformedURLException {
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-        return new ChromeDriver(options);
+        return new RemoteWebDriver(new URL(seleniumGridUrl), options);
     }
 }
